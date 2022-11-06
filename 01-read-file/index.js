5ad7cb1e2f51ca5697a4611ed51f19435c8c6a0e
Harapuchyk <harapuchyk.andrei@gmail.com>
2022-11-06 11:29:19 +0300

    fix: fix 01-read-file

--- 01-read-file/index.js
+++ 01-read-file/index.js
@@ -1,7 +1,18 @@
+// const fs = require('fs');
+// const path = require('path');
+// const stream = new fs.ReadStream(path.join(__dirname, 'text.txt'), "utf8");
+// stream.on('readable', () => {
+//     const data = stream.read();
+//     console.log(data);
+// });
 const fs = require('fs');
 const path = require('path');
-const stream = new fs.ReadStream(path.join(__dirname, 'text.txt'), "utf8");
-stream.on('readable', () => {
-    const data = stream.read();
-    console.log(data);
-});
+
+fs.readFile(
+    path.join(__dirname, 'text.txt'),
+    'utf-8',
+    (err, data) => {
+        if (err) throw err;
+        console.log(data);
+    }
+);
