1a739d96d45a8900929d155c83e6301a107dd99c
Harapuchyk <harapuchyk.andrei@gmail.com>
2022-11-07 23:35:37 +0300

    feat: finish 04-copy-directory

--- 04-copy-directory/index.js
+++ 04-copy-directory/index.js
@@ -1,1 +1,33 @@
+const fs = require('fs');
+const path = require('path');
+function copyDir () {
+    fs.access(path.join(__dirname, 'files-copy'), fs.constants.F_OK, err => {
+        if (err) {
+          //console.log(err);
+        }
+        fs.rm(path.join(__dirname, 'files-copy'), { recursive: true }, () => {
+            fs.mkdir(path.join(__dirname, 'files-copy'), err => {
+                if (err) {
+                    console.log (err);
+                }
+                fs.readdir(path.resolve(__dirname, 'files'), (err, files) => {
+                    if (err) throw err;
+                    else {    
+                        files.forEach(file => {
+                            fs.stat(path.join(__dirname, 'files', file), (err, stats) => {
+                              if (err) throw err;
+                                  fs.copyFile(path.join(__dirname, 'files', file), path.join(__dirname, 'files-copy', file), (err) => {
+                                    if (err) throw err;
+                                });
+                            });
+                        });
+                    };
+                });
+            });
+        });
+      });
+};
+copyDir();
+console.log(`files copy in files-copy`);
 
+  
